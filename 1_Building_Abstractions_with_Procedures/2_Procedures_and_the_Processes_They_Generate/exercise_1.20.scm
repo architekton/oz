7dabf8f4dd3eede141ea0d0887b3a8148036e318
Architekton <al512@protonmail.com>
2018-07-02 23:05:53 +1000

    1.19-1.21

---
+++ 1_Building_Abstractions_with_Procedures/2_Procedures_and_the_Processes_They_Generate/exercise_1.20.scm
@@ -0,0 +1,54 @@
+(define (gcd a b)
+  (if (= b 0)
+	  a
+	  (gcd b (remainder a b))))
+
+(gcd 206 40)
+
+(gcd 40 (remainder 206 40))
+
+(if (= (remainder 206 40) 0)
+	40
+	(gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+(if (= (remainder 40 (remainder 206 40)) 0)
+	(remainder 206 40)
+	(gcd (remainder 40 (remainder 206 40)) 
+		 (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+
+(gcd (remainder 40 (remainder 206 40)) 
+	 (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)
+	(remainder 40 (remainder 206 40))
+	(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+		 (remainder (remainder 40 (remainder 206 40))
+					(remainder (remainder 206 40) 
+							   (remainder 40 (remainder 206 40))))))
+
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+	 (remainder (remainder 40 (remainder 206 40))
+				(remainder (remainder 206 40) 
+						   (remainder 40 (remainder 206 40)))))
+
+(if (= (remainder (remainder 40 (remainder 206 40))
+				  (remainder (remainder 206 40) 
+							 (remainder 40 (remainder 206 40)))) 0)
+	(remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+	(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+		 (remainder (remainder 40 (remainder 206 40))
+					(remainder (remainder 206 40) 
+							   (remainder 40 (remainder 206 40))))))
+
+; With normal order, 18
+
+; With applicative order as many times as the procedure is called - 1
+; so 4 times.
+
+; (gcd 206 40)
+; (gcd 40 (remainder 206 40))
+; (gcd 6 (remainder 40 6))
+; (gcd 4 (remainder 6 4))
+; (gcd 2 (remainder 4 2))
