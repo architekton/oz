2fbfbb8b01e5a5cef03a67968918fc48fb11ec7a
Architekton <al512@protonmail.com>
2018-07-08 14:39:31 +1000

    2.30-2.32

---
+++ 2_Building_Abstractions_with_Data/1_Introduction_to_Data_Abstraction/exercise_2.32.scm
@@ -0,0 +1,13 @@
+(define nil '())
+
+; See the counting change problem, the concept is similar, the difference is
+; here rather than counting we produce the actual subsets
+
+(define (subsets s)
+  (if (null? s)
+	  (list nil)
+	  (let ((rest (subsets (cdr s))))
+		(append rest (map (lambda (x) (cons (car s) x)) rest)))))
+
+
+(subsets (list 1 2 3))
