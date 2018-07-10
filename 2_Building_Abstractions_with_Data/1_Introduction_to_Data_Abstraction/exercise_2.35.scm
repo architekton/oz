704e78c17d0cc2ad9df332f21f85c2ba6d028f38
Architekton <al512@protonmail.com>
2018-07-10 21:40:04 +1000

    2.33-2.39

---
+++ 2_Building_Abstractions_with_Data/1_Introduction_to_Data_Abstraction/exercise_2.35.scm
@@ -0,0 +1,23 @@
+(define nil '())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+	  initial
+	  (op (car sequence)
+		  (accumulate op initial (cdr sequence)))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+		((predicate (car sequence))
+		 (cons (car sequence)
+			   (filter predicate (cdr sequence))))
+		(else (filter predicate (cdr sequence)))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (curr)
+						 (if (pair? curr)
+							 (count-leaves curr)
+							 1))
+					   t)))
+
+(count-leaves (list 1 (list 2 (list 3 4))))
---
+++ 2_Building_Abstractions_with_Data/1_Introduction_to_Data_Abstraction/exercise_2.38.scm
@@ -0,0 +1,24 @@
+(define nil '())
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+	  initial
+	  (op (car sequence)
+		  (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+	(if (null? rest)
+		result
+		(iter (op result (car rest))
+			  (cdr rest))))
+  (iter initial sequence))
+
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
+
+; As we see from the above operations, they lack the commutative property. When
+; tested with addition or multiplication we distinguish that this is the case.
