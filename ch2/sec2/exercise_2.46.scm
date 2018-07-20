9a390b497d5f3fcc80123593c3adb4ebb44b6e42
Architekton <al512@protonmail.com>
2018-07-20 18:36:16 +1000

    2.46-2.48

---
+++ ch2/sec2/exercise_2.46.scm
@@ -0,0 +1,27 @@
+(define (make-vect x y)
+  (cons x y))
+
+(define xcor-vect car)
+(define ycor-vect cdr)
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+			 (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+			 (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect m v2)
+  (make-vect (* m (xcor-vect v2))
+			 (* m (ycor-vect v2))))
+
+
+(define (frame-coord-map frame)
+  (lambda (v)
+	(add-vect
+	  (origin-frame frame)
+	  (add-vect (scale-vect (xcor-vect v)
+							(edge1-frame frame))
+				(scale-vect (ycor-vect v)
+							(edge2-frame frame))))))
