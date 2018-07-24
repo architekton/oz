3787b166caee89867eb09476bee17f5fee5e34c5
Architekton <al512@protonmail.com>
2018-07-24 19:38:40 +1000

    2.61-2.62

---
+++ ch2/sec3/exercise_2.62.scm
@@ -0,0 +1,31 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+		((= x (car set)) true)
+		((< x (car set)) false)
+		(else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+	  '()
+	  (let ((x1 (car set1)) (x2 (car set2)))
+		(cond ((= x1 x2)
+			   (cons x1
+					 (intersection-set (cdr set1)
+									   (cdr set2))))
+			  ((< x1 x2)
+			   (intersection-set (cdr set1) set2))
+			  ((< x2 x1)
+			   (intersection-set set1 (cdr set2)))))))
+
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+		((null? set2) set1)
+		((= (car set1) (car set2))
+		 (cons (car set1) (union-set (cdr set1) (cdr set2))))
+		((< (car set1) (car set2))
+		 (cons (car set1) (union-set (cdr set1) set2)))
+		(else
+		  (cons (car set2) (union-set set1 (cdr set2))))))
+
+(union-set (list 1 2 3 4) (list 2 3 4 5))
