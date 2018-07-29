91b27532dd3eafdb2ceff0f69f62c74b083cdda1
Architekton <al512@protonmail.com>
2018-07-29 16:34:34 +1000

    2.74-2.76

---
+++ ch2/sec4/exercise_2.76.scm
@@ -0,0 +1,27 @@
+; Generic operation with explicit dispatch:
+; a)
+; Addition of predicates to differentiate between type tags
+; Addition of type tag for data
+; b)
+; Implement generic procedure
+
+; Data-directed:
+; a)
+; Add entries to the dispatch table based on the operations that support the
+; new type
+; b)
+; Add operation to the dispatch table
+
+; Message-passing
+; a)
+; Nothing
+; b)
+; Add operation to existing types
+
+
+; Part 2
+; a)
+; Message passing
+; b)
+; Data-directed or Message passing
+
