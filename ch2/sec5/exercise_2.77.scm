c38938bcfe8fea1f4df6ce4bbdc2f4569af5b6dc
Architekton <al512@protonmail.com>
2018-07-30 14:34:48 +1000

    2.77

---
+++ ch2/sec5/exercise_2.77.scm
@@ -0,0 +1,6 @@
+(magnitude (make-complex-from-real-imag 3 4))
+
+; apply-generic is called 2 times, dispatching for each tagged layer until
+; there are no more tags. The new put statements are required to resolve the
+; first layer from our compound structure ('complex 'rectangular 3 4), since we
+; have not defined them.
