a47537a6611108722c31d0ecc9c6046493ccf939
Architekton <al512@protonmail.com>
2018-08-13 16:58:29 +1000

    exercise_3.10.scm

---
+++ ch3/sec2/exercise_3.10.scm
@@ -0,0 +1,4 @@
+; The define syntax creates an environment E1 with the initial amount bound to it
+; as before. The let syntax creates an environment E2 which references E1, with
+; the lambda(amount) referencing E2. The difference is one extra in between
+; step, since let is infact syntactic sugar for lambda
