55c01d9f04f09aa0ec7df3c10b1194bcd1c3e881
Architekton <al512@protonmail.com>
2018-08-23 18:58:25 +1000

    fixed and added small tests

--- ch3/sec3/exercise_3.25.scm
+++ ch3/sec3/exercise_3.25.scm
@@ -4,49 +4,49 @@
     (define (lookup keys)
       (define (iter keys table)
         (cond
-          ; if we are at the last key return false nothing found
-          ((null? keys) #f)
           ; if we find a record with the last key return it else false
           ((null? (cdr keys))
            (let ((record (assoc (car keys) (cdr table))))
              (if record
                  (cdr record)
                  #f)))
+
           ; recurse with the cdr of keys
           (else
             (let ((subtable (assoc (car keys) (cdr table))))
               (if subtable
                   (iter (cdr keys) subtable)
                   #f)))))
+
       (iter keys local-table))
 
 
     (define (insert! keys value)
-      (define (create-chain keys)
+
+      (define (make-rest keys)
         (if (null? (cdr keys))
-            (cons (car keys) value)
-            (list (car keys) (create-chain (cdr keys)))))
+            (cons (car keys) '())
+            (make-rest (cdr keys))))
 
       (define (iter keys table)
-        (cond
-          ((null? (cdr keys))
-           (let ((record (assoc (car keys) (cdr table))))
+        (let ((record (assoc (car keys) (cdr table))))
+          (cond
+            ; we have reached the last key
+            ((null? (cdr keys))
              ; if the record exists replace it's value otherwise create it
              (if record
                  (set-cdr! record value)
                  (set-cdr! table
-                           (cons (cons (car keys) value) (cdr table))))))
+                           (cons (cons (car keys) value) (cdr table)))))
           (else
-            (let ((subtable (assoc (car keys) (cdr table))))
-              (if subtable
-                  ; if the subtable exists proceed to the next key
-                  (iter (cdr keys) subtable)
-                  ; otherwise create it and proceed down the chain with the
-                  ; rest of the keys and create the rest
-                  (set-cdr! table
-                            (cons (list (car keys)
-                                        (create-chain (cdr keys)))
-                                  (cdr table))))))))
+            (if record
+                (if (pair? (cdr record))
+                    (iter (cdr keys) record)
+                    (begin
+                      (set-cdr! record (cons (cons (cadr keys) '()) '()))
+                      (iter (cdr keys) record)))
+
+                (set-cdr! table (cons (make-rest keys) (cdr table))))))))
 
       (iter keys local-table)
       'ok)
@@ -57,4 +57,14 @@
             (else (error "Unknown operation -- TABLE" m))))
     dispatch))
 
-; Todo test this
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(put '(a) 1)
+(put '(a b) 2)
+(put '(a b c) 3)
+(get '(a))
+(get '(a b))
+(get '(a b c))
+
