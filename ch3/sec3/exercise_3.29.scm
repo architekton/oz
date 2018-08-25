6c47fbfa5fa8a0d2e7e8422d03cab67704159129
Architekton <al512@protonmail.com>
2018-08-25 19:05:13 +1000

    3.28-3.29

---
+++ ch3/sec3/exercise_3.29.scm
@@ -0,0 +1,41 @@
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and x y)
+  (if (and (= x 1) (= y 1))
+      1
+      0))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (or-gate a1 a2 output)
+  (let ((b (make-wire))
+        (c (make-wire))
+        (d (make-wire)))
+    (inverter a1 b)
+    (inverter a2 c)
+    (and-gate b c d)
+    (inverter d output)))
+
+; The or delay is 1 * and delay + 2 * invert delay
+
