cf0c5f2fd234275a8c6b889fb064af1517de6d93
Architekton <al512@protonmail.com>
2018-08-28 17:15:05 +1000

    3.36-3.37

---
+++ ch3/sec3/exercise_3.36.scm
@@ -0,0 +1,1 @@
+; TODO transfer diagram in asciio
---
+++ ch3/sec3/exercise_3.37.scm
@@ -0,0 +1,37 @@
+(load "constraint_system.scm")
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
+
+
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
+
+(probe "Celsius tmep" C)
+(probe "Fahrenheit temp" F)
+
+(set-value! C 25 'user)
+(forget-value! C 'user)
+(set-value! F 113 'user)
