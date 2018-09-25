1c0adc73a301f4c6e44f411a8aa9d441674bdb5c
Architekton <architekton350@gmail.com>
2018-09-25 14:42:42 +1000

    4.43-4.44

---
+++ ch4/sec3/exercise_4.43.scm
@@ -0,0 +1,1 @@
+; Same approach as the 4.42 and 4.40; TOOD
---
+++ ch4/sec3/exercise_4.44.scm
@@ -0,0 +1,5 @@
+; Construct a list of queens using amb and requires only if the new queen
+; number is valid with the previous queens. We need a function to check if a
+; new queen is in a valid position with respect to the previous queens, and to
+; require it to be true for each new queen we add. The queen position can be
+; selected with (amb 1 2 3 4 5 6 7 8)
