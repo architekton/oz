a117df239c0a912d6b36718367a1e95aff0c5874
Architekton <architekton350@gmail.com>
2018-09-25 13:35:36 +1000

    4.42

---
+++ ch4/sec3/exercise_4.42.scm
@@ -0,0 +1,18 @@
+; |   | B | E | J | K | M |
+; |---|---|---|---|---|---|
+; | B | 3 |   |   |   | 1 |
+; | E |   | 1 | 5 |   |   |
+; | J |   | 2 | 3 |   |   |
+; | K | 2 |   |   | 2 |   |
+; | M |   |   |   | 4 | 4 |
+
+; I found that with most of these puzzles approaching them with a table makes
+; them quite easy. Assume one is true and follow the rules of logic presented
+; in the question and you will either get a contradiction, or the correct
+; answer. I did this on paper and the answer is
+
+; Kitty - 1
+; Joan - 2
+; Betty - 3
+; Mary - 4
+; Ethel - 5
