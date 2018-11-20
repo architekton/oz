1c8b3f029ddcb7c0f5678eaf71169623b30e2048
Architekton <architekton350@gmail.com>
2018-11-21 10:07:27 +1100

    fix 2.83

--- ch2/sec5/exercise_2.83.scm
+++ ch2/sec5/exercise_2.83.scm
@@ -1,11 +1,16 @@
+(load "common.scm")
 
-(put 'raise 'integer
+(define numer car)
+(define denom cdr)
+
+(put 'raise '(scheme-number)
      (lambda (x) (make-rational x 1)))
 
-(put 'raise 'rational
-     (lambda (x) (make-real (/ (numer x) (denom x)))))
-
-(put 'raise 'real
-     (lambda (x) (make-from-real-imag x 0)))
+(put 'raise '(rational)
+     (lambda (x) (make-complex-from-real-imag (/ (numer x) (denom x)) 0)))
 
 (define (raise x) (apply-generic 'raise x))
+
+(define n1 (make-scheme-number 1))
+(raise n1)
+(raise (raise n1))
