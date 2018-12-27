940010faf472473e4cb7fb78ff30d5e60d4461d2
Architekton <architekton350@gmail.com>
2018-12-27 21:24:28 +1100

    fix 2.89

--- ch2/sec5/exercise_2.89.scm
+++ ch2/sec5/exercise_2.89.scm
@@ -2,19 +2,15 @@
 (load "symbolic_algebra.scm")
 
 (define (adjoin-term term term-list)
-  (let* ((ord (order term))
-        (coe (coeff term)))
-    (define (iter left terms)
-      (cond ((null? terms)
-             (list coe))
-            ((= left 0)
-             (cons coe terms))
+      (cond ((= (coeff term) 0)
+             term-list)
+            ((= (order term) (length term-list))
+             (cons (coeff term) term-list))
             (else
-              (iter (- left 1) terms))))
-    (iter ord term-list)))
+              (adjoin-term term (cons 0 term-list)))))
 
 (define (the-empty-termlist) '())
-(define (first-term term-list) (car term-list))
+(define (first-term term-list) (make-term (- (length term-list) 1 ) (car term-list)))
 (define (rest-terms term-list) (cdr term-list))
 (define (empty-termlist? term-list) (null? term-list))
 
@@ -23,10 +19,9 @@
 (define (coeff term) (cadr term))
 
 ; Book example test
-(define t (adjoin-term (make-term 5 1) (the-empty-termlist)))
-(define t (adjoin-term (make-term 4 2) t))
-(define t (adjoin-term (make-term 3 0) t))
-(define t (adjoin-term (make-term 2 3) t))
+(define t (adjoin-term (make-term 0 -5) (the-empty-termlist)))
 (define t (adjoin-term (make-term 1 -2) t))
-(define t (adjoin-term (make-term 0 -5) t))
+(define t (adjoin-term (make-term 2 3) t))
+(define t (adjoin-term (make-term 4 2) t))
+(define t (adjoin-term (make-term 5 1) t))
 t
